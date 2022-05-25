0a4963cabec3712b6961536bd397cb9298874761
pradeeps4ini <pradeepsaini@protonmail.com>
2022-05-25 17:03:08 +0530

    books id in library and dom updated once a book is deleted

--- script.js
+++ script.js
@@ -1,103 +1,159 @@
-"use strict"
+"use strict";
 
 
 const domElements = function() {
-  const dialogModal = document.getElementById("bookDetailsModal");
-  const addBookBtn = document.querySelector(".addBook");
-  const cancelFormBtn = document.querySelector(".cancelButton");
-  const submitFormBtn = document.querySelector(".submitBook");
+  const bookInputModal = document.getElementById("bookInputModal");
+  const addNewBookBtn = document.querySelector(".addNewBook");
+  const bookInputCancelBtn = document.querySelector(".bookInputCancelBtn");
+  const submitBook = document.getElementById("submitBook");
   const authorName = document.getElementById("authorName");
   const bookName = document.getElementById("bookName");
   const bookPages = document.getElementById("bookPages");
-  const isBookRead = document.getElementById("isBookRead");
-  const bookCardUi = document.querySelector(".bookCardUi");
-  const bookCards = document.querySelector(".bookCards");
-  const bookList = document.getElementById("bookList");
+  const bookReadStatus = document.getElementById("isBookRead");
+  const bookListElement = document.getElementById("bookList");
 
-  const bookInput = {authorName, bookName, bookPages, isBookRead}; 
-  const card = bookCardUi.cloneNode("true");
-  card.classList.remove("hidden")
-  return {bookInput, addBookBtn, cancelFormBtn, submitFormBtn, dialogModal, card, bookCards, bookList};
-};
+  const bookInputValues = {authorName, bookName, bookPages, bookReadStatus};
 
-
-const shiftBookId = function() {
-  myLibrary = myLibrary.map((item) => {
-
-    let newId = (item.id === 0) ? item.id : item.id - 1;
-    item = {authorName: item.authorName, bookName: item.bookName, bookPages: item.bookPages, isBookRead: item.isBookRead, id: newId}
-    return item;
-  })
-};
-
-
-const deleteBookFromLibrary = function() {
-  const bookId = event.target.parentNode.classList[0];
-  console.log(event.target.parentNode)
-  console.log(bookId, "bookId");
-  const findBook = document.getElementById(bookId);
-  findBook.remove();
-  myLibrary.splice(bookId, 1);
-  shiftBookId();
+  return {bookInputModal, addNewBookBtn, bookInputCancelBtn, submitBook, 
+    bookInputValues, bookListElement};
 }
 
 
-const createBookCard = function(bookDetails) {
-  const {card, bookCards, bookList} = domElements();
+const createElement = function(element) {
+  return document.createElement(element);
+} 
 
-  const {0: deleteBook, 1: authorName, 2: bookName, 3: bookPages, 4: isBookRead} = card.children;
+
+const makeBookCardUi = function() {
+  const bookCardDiv = createElement("div");
   
-  authorName.children[1].textContent = bookDetails.authorName;
-  bookName.children[1].textContent = bookDetails.bookName;
-  bookPages.children[1].textContent = bookDetails.bookPages;
-  isBookRead.children[1].textContent = (bookDetails.isBookRead === true) ? "Yes" : "No";
-  deleteBook.children[0].addEventListener("click", () => deleteBookFromLibrary()); 
-  deleteBook.children[0].setAttribute("class", bookDetails.id);
+  const authorNamePara = createElement("p");
+  const bookNamePara = authorNamePara.cloneNode(true);
+  const bookPagesPara = authorNamePara.cloneNode(true);
+  const toggleReadStatusBtn = authorNamePara.cloneNode(true);
 
-  card.setAttribute("id", bookDetails.id)
-  bookCards.appendChild(card);
-  bookList.appendChild(bookCards);
+  const deleteImg = createElement("img");
+  const deleteBtn = createElement("button");
+  deleteImg.setAttribute("src", "./images/bin.png");
+  deleteImg.setAttribute("alt", "delete book button");
+  deleteBtn.appendChild(deleteImg);
+  deleteBtn.setAttribute("class", "deleteBookCard");
+  deleteBtn.setAttribute("type", "button");
+  deleteBtn.addEventListener("click", deleteBookCard);
+
+  [deleteBtn, authorNamePara, bookNamePara, bookPagesPara, toggleReadStatusBtn].
+    forEach((item) => bookCardDiv.appendChild(item));
+
+  return bookCardDiv;
 }
 
 
-const Book = function(authorName="unknown", bookName="unknown", bookPages=0, isBookRead=false, id) {
-  return {authorName, bookName, bookPages, isBookRead, id};
-}
+const CreateLibrary = function() {
+  this.allBooks = [];
+
+  this.addBook = function(newBook) {
+    if (!(newBook in this.allBooks)) {
+      this.allBooks.push(newBook);
+    }  
+  };
+
+  this.removeBook = function(bookId) {
+    this.allBooks.splice(bookId - 1, 1);
+  };
+
+  this.updateBookReadStatus = function(bookId) {
+    this.allBooks.forEach((book) => {
+      if (book.id === bookId) {
+        book.readStatus = !book.readStatus;}
+    })
+  };
+
+  this.shiftBooksId = function(lastRemovedBookId) {
+    let allBooksLength = this.allBooks.length;
+   
+    for (let i= lastRemovedBookId - 1; i< allBooksLength; i+= 1) {
+      this.allBooks[i].id -= 1;  
+    }
+  };
+
+  this.generateBookId = function() {
+    const id = (this.allBooks.length) + 1;
+    return id;
+  };
+}; 
 
 
-const createLibrary = function() {
-  let library = [];
-  let id = 0;  
-  const addBookToMyLibrary = function(...args) {
-    const [authorName, bookName, bookPages, isBookRead] = [...args];
-    const newBook = Book(authorName, bookName, bookPages, isBookRead, id);
-    id += 1;
+const library = new CreateLibrary();
 
-    library.push(newBook);
-    createBookCard(newBook);
-    
-    console.log(library);
+
+const updateDomBookList = function(domBookList, bookId) {
+  const domBookLength = domBookList.children.length;
+
+  for (let i= bookId - 1; i < domBookLength; i+= 1) {
+    domBookList.children[i].className = i + 1; 
   }
-  return [library, addBookToMyLibrary];
 }
 
 
-let [myLibrary, addBookToMyLibrary] = createLibrary();
+const deleteBookCard = function() {
+  let bookDiv = (event.target === event.currentTarget) ? 
+                event.target.parentNode :
+                event.currentTarget.parentNode;
+  
+  let bookList = bookDiv.parentNode;
+  let bookId = +bookDiv.className;
+  bookDiv.remove();
+  library.removeBook(bookId);
+  library.shiftBooksId(bookId);
+  updateDomBookList(bookList, bookId)
+}
+
+
+const Book = function({authorName, bookName, bookPages, bookReadStatus, id}) {
+  return {authorName, bookName, bookPages, bookReadStatus, id};
+}
+
+
+const publishBook = function(newBook, bookCardDiv,  bookListElement) {
+  bookCardDiv.children[1].textContent = `Author: ${newBook.authorName}`;
+  bookCardDiv.children[2].textContent = `Book: ${newBook.bookName}`;
+  bookCardDiv.children[3].textContent = `Pages: ${newBook.bookPages}`;
+  bookCardDiv.children[4].textContent = `Read? ${newBook.bookReadStatus}`;
+  bookCardDiv.classList.add(newBook.id); 
+  bookListElement.appendChild(bookCardDiv);
+}
+
+
+const createBook = function(bookInputValues, bookCardDiv, bookListElement) {
+  const bookId = library.generateBookId();
+  bookInputValues.id = bookId;
+  const newBook = Book({...bookInputValues});
+  library.addBook(newBook);
+  publishBook(newBook, bookCardDiv, bookListElement);
+}
 
 
 const domInteractions = function() {
-  let {bookInput, addBookBtn, cancelFormBtn, submitFormBtn, dialogModal, deleteBook} = domElements();
+  let {bookInputModal, addNewBookBtn, bookInputCancelBtn, submitBook, 
+    bookInputValues, bookListElement} = domElements();
+  
 
-  addBookBtn.addEventListener("click", () => dialogModal.showModal());
+  addNewBookBtn.addEventListener("click",  () => bookInputModal.showModal());
+  
+  bookInputCancelBtn.addEventListener("click", () => bookInputModal.close());
 
-  cancelFormBtn.addEventListener("click", () => dialogModal.close());
+  submitBook.addEventListener("click", () => {
+    const {authorName, bookName, bookPages, bookReadStatus} = {...bookInputValues};
+    const bookValues = {authorName: authorName.value, 
+                             bookName: bookName.value, 
+                             bookPages: bookPages.value, 
+                             bookReadStatus: bookReadStatus.checked};
+    const bookCardDiv = makeBookCardUi();
 
-
-  submitFormBtn.addEventListener("click", () => {
-    dialogModal.close();
-    let {authorName, bookName, bookPages, isBookRead} = bookInput;
-    addBookToMyLibrary(authorName.value, bookName.value, bookPages.value, isBookRead.checked);
+    bookInputModal.close();   
+    createBook(bookValues, bookCardDiv, bookListElement);
   });
-}
+};
+
 
 domInteractions();
